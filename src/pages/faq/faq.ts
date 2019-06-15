d48710bc6f4b82cd309897c2910118e1a45dff7d
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-15 15:09:06 +0200

    adding FAQ Page

---
+++ src/pages/faq/faq.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-faq',
+  templateUrl: 'faq.html',
+})
+export class FaqPage {
+  fromLogin: boolean;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams)
+  {
+    this.fromLogin = false;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FaqPage');
+  }
+
+  ionViewCanEnter(){
+    this.fromLogin = this.navParams.get('fromLogin');
+  }
+
+}
